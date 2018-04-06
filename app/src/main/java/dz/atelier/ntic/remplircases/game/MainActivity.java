83efbfc92412f1ad724d514ce4b40e4d9f0dda36
Youcef Megoura <meg.youcef@yahoo.fr>
2018-04-06 01:23:00 +0100

    first

--- app/src/main/java/dz/atelier/ntic/remplircases/game/MainActivity.java
+++ app/src/main/java/dz/atelier/ntic/remplircases/game/MainActivity.java
@@ -1,20 +1,64 @@
 package dz.atelier.ntic.remplircases.game;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    Intent inte;
+    EditText id, login , mdp;
+    Button bcreation;
+    private FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            login = (EditText)findViewById(R.id.login);
+            mdp = (EditText)findViewById(R.id.mdp);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        bcreation = (Button)findViewById(R.id.logID);
     }
 
+    public void userLogin() {
+        String username = login.getText().toString().trim();
+        String password = mdp.getText().toString().trim();
+
+        mAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent myintent= new Intent(MainActivity.this,CategoriesActivity.class);
+                    startActivity(myintent);
+
+                }else{
+                    Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
     public void LoginClick(View view) {
-        Intent myintent= new Intent(this,CategoriesActivity.class);
+        userLogin();
+    }
+
+    public void SignUpClick(View view) {
+        Intent myintent= new Intent(this,SignUp.class);
         startActivity(myintent);
     }
 }
